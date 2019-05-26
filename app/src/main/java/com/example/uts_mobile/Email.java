d7bd7032a722be2dfd0a86502e7b0b1aa885752a
Deb2Roy <faray67@gmail.com>
2019-05-26 19:47:49 +0700

    UTS

---
+++ app/src/main/java/com/example/uts_mobile/Email.java
@@ -0,0 +1,105 @@
+package com.example.uts_mobile;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
+
+public class Email extends AsyncTask <String, Void, String> {
+    AlertDialog dialog;
+    Context context;
+    public Boolean login = false;
+
+    public Email(Context context){
+        this.context=context;
+    }
+
+    @Override
+    protected void onPreExecute() {
+        dialog = new AlertDialog.Builder(context).create();
+        dialog.setTitle("Status");
+        dialog.setCancelable(true);
+    }
+
+    @Override
+    protected void onPostExecute(String s) {
+        dialog.setMessage(s);
+        dialog.show();
+        if(s.contains("Berhasil"))
+        {
+            dialog.dismiss();
+            Toast.makeText(context,"Data Berhasil di kirim", Toast.LENGTH_SHORT).show();
+            /*Intent intent_name = new Intent();
+            intent_name.setClass(context.getApplicationContext(),Main2Activity.class);
+            context.startActivity(intent_name);*/
+        }else{
+            dialog.dismiss();
+            Toast.makeText(context,"Data Gagal di kirim", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+    @Override
+    protected String doInBackground(String... voids) {
+        String result = "";
+        String NIM = voids[0];
+        String nama = voids[1];
+        String kelas = voids[2];
+        String email = voids[3];
+
+
+        String connstr = "http://nurchim.000webhostapp.com/UTS_Mobile/UTSmobilekamis.php/";
+
+        try {
+            URL url = new URL(connstr);
+            HttpURLConnection http = (HttpURLConnection) url.openConnection();
+            http.setRequestMethod("POST");
+            http.setDoInput(true);
+            http.setDoOutput(true);
+
+            OutputStream ops = http.getOutputStream();
+            BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(ops,"UTF-8"));
+            String data = URLEncoder.encode("NIM","UTF-8")+"="+ URLEncoder.encode(NIM,"UTF-8")
+                    +"&&"+ URLEncoder.encode("nama","UTF-8")+"="+ URLEncoder.encode(nama,"UTF-8")
+                    +"&&"+ URLEncoder.encode("kelas","UTF-8")+"="+ URLEncoder.encode(kelas,"UTF-8")
+                    +"&&"+ URLEncoder.encode("email","UTF-8")+"="+ URLEncoder.encode(email,"UTF-8");
+            writer.write(data);
+            writer.flush();
+            writer.close();
+            ops.close();
+
+            InputStream ips = http.getInputStream();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(ips,"ISO-8859-1"));
+            String line ="";
+            while ((line = reader.readLine()) != null)
+            {
+                result += line;
+            }
+            reader.close();
+            ips.close();
+            http.disconnect();
+            return result;
+
+        } catch (MalformedURLException e) {
+            result = e.getMessage();
+        } catch (IOException e) {
+            result = e.getMessage();
+        }
+
+
+        return result;
+    }
+}
---
+++ app/src/main/java/com/example/uts_mobile/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.uts_mobile;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    EditText ediNim, ediNama, ediKelas, ediEmail;
+    Button btnEmail;
+    Email emaill;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        emaill = new Email(this);
+
+        ediNim = (EditText)findViewById(R.id.edinim);
+        ediNama = (EditText)findViewById(R.id.edinama);
+        ediKelas = (EditText)findViewById(R.id.edikelas);
+        ediEmail = (EditText)findViewById(R.id.ediemail);
+        btnEmail = (Button)findViewById(R.id.btnEmail);
+
+        btnEmail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String nim = ediNim.getText().toString();
+                String nama = ediNama.getText().toString();
+                String kelas = ediKelas.getText().toString();
+                String email = ediEmail.getText().toString();
+                if (nim.isEmpty() || nama.isEmpty() || kelas.isEmpty() || email.isEmpty()) {
+                    Toast.makeText(getApplicationContext(), "Semua Harus Diisi", Toast.LENGTH_SHORT).show();
+                } else emaill.execute(nim, nama, kelas, email);
+
+            }
+        });
+    }
+}
